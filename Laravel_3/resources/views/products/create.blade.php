68668b3bdddd997dc1960c5841f0983da8bf3893
carlos-paezf <carlos.paezf@usantoto.edu.co>
2020-07-23 13:37:22 -0500

    Formulario con Laravel Collective

--- Laravel_3/resources/views/products/create.blade.php
+++ Laravel_3/resources/views/products/create.blade.php
@@ -5,44 +5,55 @@
     @endsection
 
     @section("content")
-        <form method="post" action="/products">
+        <!--<form method="post" action="/products">-->
+        {!! Form::open(['url' => '/products', 'method' => 'post']) !!}
             <table>
                 <tr>
-                    <td>Nombre:&nbsp;</td>
+                    <!--<td>Nombre:&nbsp;</td>-->
+                    <td>{!! Form::label('article_name', 'Nombre Articulo:') !!}</td>
                     <td>
-                        <input type="text" name="article_name">
+                        <!--<input type="text" name="article_name">-->
+                        {!! Form::text('article_name') !!}
                         {{csrf_field()}}
                     </td>
                 </tr>
 
                 <tr>
-                    <td>Seccion:&nbsp;</td>
+                    <!--<td>Seccion:&nbsp;</td>-->
+                    <td>{!! Form::label('section', 'Seccion:') !!}</td>
                     <td>
-                        <input type="text" name="section">
+                        <!--<input type="text" name="section">-->
+                        {!! Form::text('section') !!}
                         {{csrf_field()}}
                     </td>
                 </tr>
 
                 <tr>
-                    <td>Precio:&nbsp;</td>
+                    <!--<td>Precio:&nbsp;</td>-->
+                    <td>{!! Form::label('price', 'Precio:') !!}</td>
                     <td>
-                        <input type="text" name="price">
+                        <!--<input type="text" name="price">-->
+                        {!! Form::text('price') !!}
                         {{csrf_field()}}
                     </td>
                 </tr>
 
                 <tr>
-                    <td>Date:&nbsp;</td>
+                    <!--<td>Date:&nbsp;</td>-->
+                    <td>{!! Form::label('date', 'Date:') !!}</td>
                     <td>
-                        <input type="text" name="date">
+                        <!--<input type="date" name="date">-->
+                        {!! Form::date('date') !!}
                         {{csrf_field()}}
                     </td>
                 </tr>
 
                 <tr>
-                    <td>Fabricado en:&nbsp;</td>
+                    <!--<td>Fabricado en:&nbsp;</td>-->
+                    <td>{!! Form::label('made_in', 'Fabricado en:') !!}</td>
                     <td>
-                        <input type="text" name="made_in">
+                        <!--<input type="text" name="made_in">-->
+                        {!! Form::text('made_in') !!}
                         {{csrf_field()}}
                     </td>
                 </tr>
@@ -53,7 +64,8 @@
                 </tr>
 
             </table>
-        </form>
+        <!--</form>-->
+        {!! Form::close() !!}
     @endsection
 
     @section("footer")
