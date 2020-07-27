47518b1ca500073ee2d641d358d1712f76ab3e65
carlos-paezf <carlos.paezf@usantoto.edu.co>
2020-07-27 16:47:25 -0500

    Vistas previas

---
+++ Full_Application/resources/views/admin/users/create.blade.php
@@ -0,0 +1,12 @@
+<html>
+
+    <head>
+        <meta http-equiv="content-type" charset="utf-8" />
+        <title></title>
+    </head>
+
+    <body>
+        <h1>Página de Creación, Admin</h1>
+    </body>
+
+</html>
---
+++ Full_Application/resources/views/admin/users/index.blade.php
@@ -0,0 +1,12 @@
+<html>
+
+    <head>
+        <meta http-equiv="content-type" charset="utf-8" />
+        <title></title>
+    </head>
+
+    <body>
+        <h1>Página de Bienvenida, Admin</h1>
+    </body>
+
+</html>
