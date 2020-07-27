29fb6edbec99032d673c1ff08459fcd56dd5e957
carlos-paezf <carlos.paezf@usantoto.edu.co>
2020-07-27 16:47:10 -0500

    Asignacion de rutas para administrador

--- Full_Application/routes/web.php
+++ Full_Application/routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+/*Rutas para Administrador*/
+Route::resource('admin/users', 'AdminUsersController');
