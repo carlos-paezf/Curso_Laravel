05b3a78248cb318e04734bfdd0b43d096770cd1b
carlos-paezf <carlos.paezf@usantoto.edu.co>
2020-07-27 17:12:17 -0500

    Listar Usuarios de la BBDD

--- Full_Application/resources/views/admin/users/index.blade.php
+++ Full_Application/resources/views/admin/users/index.blade.php
@@ -7,6 +7,29 @@
 
     <body>
         <h1>Página de Bienvenida, Admin</h1>
+
+        <table width="800" border="1">
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Role ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Created at</th>
+                <th scope="col">Updated at</th>
+            </tr>
+            @if($users)
+                @foreach ($users as $user)
+                    <tr>
+                        <td>{{$user->id}}</td>
+                        <td>{{$user->role_id}}</td>
+                        <td>{{$user->name}}</td>
+                        <td>{{$user->email}}</td>
+                        <td>{{$user->created_at}}</td>
+                        <td>{{$user->updated_at}}</td>
+                    </tr>
+                @endforeach
+            @endif
+        </table>
     </body>
 
 </html>
