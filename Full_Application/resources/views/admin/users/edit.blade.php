ed67597ddf9b2995ef5c3beb4142d694518673ba
carlos-paezf <carlos.paezf@usantoto.edu.co>
2020-07-28 13:54:33 -0500

    Maquillando con Bootstrap

--- Full_Application/resources/views/admin/users/edit.blade.php
+++ Full_Application/resources/views/admin/users/edit.blade.php
@@ -3,16 +3,17 @@
     <head>
         <meta http-equiv="content-type" charset="utf-8" />
         <title></title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     </head>
 
     <body>
-        <h1>Página de Edicion, Admin</h1>
+        <h1 style="text-align:center; margin:50px">Página de Edicion, Admin</h1>
 
         {!! Form::model($user, ['method' => 'PATCH',
                                 'action' => ['AdminUsersController@update',
                                                 $user->id],
                                 'files' => true]) !!}
-            <table width="500">
+            <table width="500" class="table">
                 <tr>
                     <td>{!! Form::label('role_id', 'Role ID:') !!}</td>
                     <td>{!! Form::text('role_id') !!}</td>
@@ -48,7 +49,10 @@
                                                 $user->id]]) !!}
             {!! Form::submit('Eliminar Usuario') !!}
         {!! Form::close() !!}
-        
+
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
     </body>
 
 </html>
--- Full_Application/resources/views/admin/users/index.blade.php
+++ Full_Application/resources/views/admin/users/index.blade.php
@@ -3,12 +3,13 @@
     <head>
         <meta http-equiv="content-type" charset="utf-8" />
         <title></title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     </head>
 
     <body>
-        <h1>Página de Bienvenida, Admin</h1>
+        <h1 style="text-align:center; margin:50px">Página de Bienvenida, Admin</h1>
 
-        <table width="1200" border="1">
+        <table width="1200" class="table">
             <tr>
                 <th scope="col">ID</th>
                 <th scope="col">Role ID-Name</th>
@@ -36,6 +37,10 @@
                 @endforeach
             @endif
         </table>
+
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
     </body>
 
 </html>
